97355795deda63ca01517fc81030e1c15f36d2b6
Scotthunt0413 <hunts10@southernct.edu>
2023-12-05 19:32:12 -0500

    fixed files

--- Website/models.py
+++ Website/models.py
@@ -1,37 +1,18 @@
 from flask import current_app
 from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, redirect, url_for, session, Blueprint, flash, current_app
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
-def create_users_table():
-    # Ensure that the 'mysql' extension is available in the app context
-    if 'mysql' not in current_app.extensions:
-        raise RuntimeError("MySQL extension not initialized. Call MySQL.init_app(app) first.")
-
-    # Get the MySQL connection
-    mysql = current_app.extensions['mysql']
-
-    # Create a cursor to execute SQL queries
-    cursor = mysql.connection.cursor()
-
-    # Define the SQL query to create a 'users' table
-    create_users_table_query = """
-    CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-    );
-"""
-
+def create_tables():
+    mysql = current_app.mysql
     try:
-        # Execute the query to create the 'users' table
-        cursor.execute(create_users_table_query)
-        # Commit the changes to the database
-        mysql.connection.commit()
+        mysql.connection.ping()
     except Exception as e:
-    # Rollback the changes in case of an error
-        mysql.connection.rollback()
-        print(f"Error creating tables: {e}")
-    finally:
-        # Close the cursor
-        cursor.close()
-
+        print(f"Error pinging MySQL: {e}")
+        return 
+    with current_app.app_context():
+        cur = mysql.connection.cursor()
+        cur.close()
+        mysql.connection.commit()
